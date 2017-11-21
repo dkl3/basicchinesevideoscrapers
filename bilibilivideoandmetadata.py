ec1391768683cb645f8371928d9d87886ddddf40
Gazlene <dkl3@users.noreply.github.com>
2017-11-20 17:59:03 -0800

    Update bilibilivideoandmetadata.py

--- bilibilivideoandmetadata.py
+++ bilibilivideoandmetadata.py
@@ -3,4 +3,4 @@ import os
 id = input("Enter the BiliBili identifier, begins with \'av\': ")
 
 os.system('you-get ' + 'http://www.bilibili.com/video/' + id)
-os.system('youtube-dl ' + 'http://www.bilibili.com/video/' + id + ' --write-json --write-thumbnail --write-description --write-annotations --skip-download')
+os.system('youtube-dl ' + 'http://www.bilibili.com/video/' + id + ' --write-info-json --write-thumbnail --write-description --write-annotations --skip-download')
