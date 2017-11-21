5839fe08c3d0ccd5c193b2a1a2ac29525b697735
Gazlene <dkl3@users.noreply.github.com>
2017-11-20 20:19:25 -0800

    Add files via upload
    Tudou scraper

---
+++ tudouscraper.py
@@ -0,0 +1,47 @@
+import sys
+import os
+import re
+import urllib.request
+import wget
+from bs4 import BeautifulSoup
+
+vidid = input("Enter a Tudou identifier: ")
+url = 'http://video.tudou.com/v/' + vidid + '.html'
+page = urllib.request.urlopen(url)
+soup = BeautifulSoup(page, 'html.parser')
+
+title = soup.find('span', attrs={'id': 'subtitle'})['title']
+
+uploader = soup.find('a', attrs={'class': 'td-play__userinfo__name'})
+uploadername = uploader.text.strip()
+channellink = 'http:' + uploader['href']
+description = soup.find('div', attrs={'class': 'td-play__videoinfo__details-box__desc'}).text.strip()
+uploaddate = soup.find('meta', attrs={'name': 'publishedtime'})['content']
+
+print('Uploader: ' + uploadername)
+print('Channel link: ' + channellink)
+print('Description: ' + description)
+print('Upload date: ' + uploaddate)
+print('Original url: ' + url)
+
+uploaderout = 'Uploader: ' + uploadername
+channelout = 'Channel: ' + channellink
+descout = 'Description: ' + description
+uploadout = 'Upload date: ' + uploaddate
+urlout = 'Original url: ' + url
+
+print ('Downloading thumbnail...')
+thumbloc = soup.find('meta', attrs={'name': 'thumb'})['content']
+thumbdown = wget.download(thumbloc)
+os.rename(thumbdown,title+'.jpg')
+
+textfile = title + "-" + vidid + '-metadata.txt'
+textfile = textfile.translate(str.maketrans("*/\\<>:\"|","--------")).strip()
+variableprintstring = (uploaderout + "\n" + channelout + "\n" + descout + "\n" + uploadout + "\n" + urlout)
+f = open( textfile, 'w' )
+f.write(variableprintstring + "\n")
+f.close()
+
+print('\n' + 'Downloading video...')
+os.system('you-get ' + url)
+print('Done!')
