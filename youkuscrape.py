c2fee01d3d3142c26cc44b3352a7b7b1649be045
Gazlene <dkl3@users.noreply.github.com>
2017-11-20 21:32:01 -0800

    Add files via upload

--- youkuscrape.py
+++ youkuscrape.py
@@ -11,40 +11,38 @@ url = 'http://www.29bc.com/plus/youku/index.php?id=' + vidid
 page = urllib.request.urlopen(url)
 soup = BeautifulSoup(page, 'html.parser')
 
-desc_get = soup.find('div', attrs={'class': 'panel-body info-content'})
-desc = desc_get.text.strip()
+desc = soup.find('div', attrs={'class': 'panel-body info-content'}).text.strip()
 
-get_info = soup.find('ul', attrs={'class': 'list-group info-content'})
-info = get_info.text.strip()
+info = soup.find('ul', attrs={'class': 'list-group info-content'}).text.strip()
 
-get_uploaded = get_info.find("span", {"class": "v-published", "yk": "video-published"})
-uploaded = get_uploaded.text.strip()
+uploaded = get_info.find("span", {"class": "v-published", "yk": "video-published"}).text.strip()
 
-get_uploader = get_info.find("a", {"class": "v-user", "yk": "user-name"})
-uploader = get_uploader.text.strip()
+uploader = get_info.find("a", {"class": "v-user", "yk": "user-name"}).text.strip()
+
+get_title = soup.find("strong", {"class": "v-title vtitle", "yk": "video-title"})
+title = get_title.text.strip()
 
 source = 'http://v.youku.com/v_show/id_' + vidid + '.html'
 
+print("Title: " + title)
 print("Uploader: " + uploader)
 print("Upload date: " + uploaded) # In YYYY-MM-DD format.
 print("Description: " + desc)
 print("Original url: " + source)
 
+title_output = "Title: " + title
 uploader_output = "Uploader: " + uploader
 uploaded_output = "Upload date: " + uploaded
 desc_output = "Description: " + desc
 source_output = "Original url: " + source
 
-get_title = soup.find("strong", {"class": "v-title vtitle", "yk": "video-title"})
-title = get_title.text.strip()
-
 textfile = title + "-" + vidid + '-metadata.txt'
 
-variableprintstring = (uploader_output + "\n" + uploaded_output + "\n" + desc_output + "\n" + source_output )
+variableprintstring = (title_output + "\n" + uploader_output + "\n" + uploaded_output + "\n" + desc_output + "\n" + source_output )
 f = open( textfile, 'w' )
 f.write(variableprintstring + "\n")
 f.close()
 
-
 print('Downloading video...')
 os.system('you-get ' + source)
+print('Done!')
