360420ce3f1f9250420b28654608cf9b9aedcd6c
Gazlene <dkl3@users.noreply.github.com>
2018-01-07 16:42:23 -0800

    Add files via upload

--- tudouscraper.py
+++ tudouscraper.py
@@ -6,15 +6,15 @@ import wget
 import urllib.request
 from bs4 import BeautifulSoup
 
-vidid = input("Enter a Tudou identifier: ")
-url = 'http://video.tudou.com/v/' + vidid + '.html'
-fnameappend = '.info.json'
-jsonenc = json.JSONEncoder()
-page = urllib.request.urlopen(url)
-soup = BeautifulSoup(page, 'html.parser')
-title = soup.find('span', attrs={'id': 'subtitle'})['title']
+def Tudou(url):
+    vidid = re.search(r'(?:\/v\/)(.*)(\.html)', url, re.I).group(1)
+    url = 'http://video.tudou.com/v/' + vidid + '.html'
+    fnameappend = '.info.json'
+    jsonenc = json.JSONEncoder()
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser')
+    title = soup.find('span', attrs={'id': 'subtitle'})['title']
 
-def Tudou():
     dict = {'vidid': vidid, \
             'origurl': url, \
             'title': title, \
@@ -23,23 +23,18 @@ def Tudou():
             'channel': None, \
             'uploaded': None}
 
-    dict['description'] = soup.find('div', attrs={'class': 'td-play__videoinfo__details-box__desc'}).text.strip()
     uploader = soup.find('a', attrs={'class': 'td-play__userinfo__name'})
     dict['uploader'] = uploader.text.strip()
     dict['channel'] = 'http:' + uploader['href']
+    dict['description'] = soup.find('div', attrs={'class': 'td-play__videoinfo__details-box__desc'}).text.strip()
     dict['uploaded'] = soup.find('meta', attrs={'name': 'publishedtime'})['content']
 
     print('Title: ' + dict['title'])
     print('Uploader: ' + dict['uploader'])
     print('Channel link: ' + dict['channel'])
-    print('Upload date: ' + dict['uploaded'])
     print('Description: ' + dict['description'])
+    print('Upload date: ' + dict['uploaded'])
     print('Original url: ' + dict['origurl'])
-
-    print ('Downloading thumbnail...')
-    thumbloc = soup.find('meta', attrs={'name': 'thumb'})['content']
-    thumbdown = wget.download(thumbloc)
-    os.rename(thumbdown,title+'.jpg')
 
     filename=title.translate(str.maketrans("*/\\<>:\"|","--------")).strip()+"-" + vidid + fnameappend
     print(filename)
@@ -47,8 +42,7 @@ def Tudou():
     f = open(filename, 'w')
     f.write(jsonenc.encode({dict['vidid']: dict}))
     f.close()
-Tudou()
 
-print('\n' + 'Downloading video...')
-os.system('you-get ' + url + ' -O "' + title + '"-' + vidid)
-print('Done!')
+    vidname = title.translate(str.maketrans("*/\\<>:\"|","--------")).strip()+"-" + vidid
+
+    return vidname
