b4e973bc6a5f6a237aa46d10887f888b6f785807
Gazlene <dkl3@users.noreply.github.com>
2018-03-29 14:05:34 +0000

    Add files via upload

--- youkuscrape.py
+++ youkuscrape.py
@@ -1,5 +1,3 @@
-# Metadata is sourced from 29bc.com/plus/youku since it displays improperly on the desktop version of Youku.
-
 import sys
 import os
 import re
@@ -10,40 +8,30 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 
 vidid = input("Enter a Youku identifier: ")
-url = 'http://www.29bc.com/plus/youku/index.php?id=' + vidid
-source = 'http://v.youku.com/v_show/id_' + vidid + '.html'
+url = 'http://v.youku.com/v_show/id_' + vidid + '.html'
 fnameappend = '.info.json'
 jsonenc = json.JSONEncoder()
 page = urllib.request.urlopen(url)
 soup = BeautifulSoup(page, 'html.parser')
-title = soup.find("strong", {"class": "v-title vtitle", "yk": "video-title"}).text.strip()
+title = soup.find("meta", {"name": "irTitle"})['content']
 
 def Youku(url):
     dict = {'vidid': vidid, \
-            'origurl': source, \
+            'origurl': url, \
             'title': title, \
-            'description': None, \
             'uploader': None, \
             'channel': None, \
             'uploaded': None}
 
-    browser = webdriver.PhantomJS() # be sure to sudo apt install phantomjs first!
-    browser.get(source)
-    soup2 = BeautifulSoup(browser.page_source, 'html.parser')
-
-    dict['channel'] = 'http:' + soup2.find('dl', attrs={'id': 'subscription_wrap'}).a['href']
-    dict['description'] = soup.find('div', attrs={'class': 'panel-body info-content'}).text.strip()
-    get_info = soup.find('ul', attrs={'class': 'list-group info-content'})
-    info = get_info.text.strip()
-    dict['uploaded'] = get_info.find("span", {"class": "v-published", "yk": "video-published"}).text.strip()
-    dict['uploader'] = get_info.find("a", {"class": "v-user", "yk": "user-name"}).text.strip()
+    dict['channel'] = 'http:' + soup.find('span', attrs={'id': 'module_basic_sub'}).a['href']
+    dict['uploaded'] = soup.find("span", {"class": "bold mr3"}).text.strip().replace('上传于 ','')
+    dict['uploader'] = soup.find("span", {"id": "module_basic_sub"}).text.strip()
 
     print("Title: " + title)
     print("Uploader: " + dict['uploader'])
     print("Channel: " + dict['channel'])
     print("Upload date: " + dict['uploaded']) # In YYYY-MM-DD format.
-    print("Description: " + dict['description'])
-    print("Original url: " + source)
+    print("Original url: " + url)
 
     filename=title.translate(str.maketrans("*/\\<>:\"|","--------")).strip()+"-" + vidid + fnameappend
     print(filename)
@@ -54,5 +42,5 @@ def Youku(url):
 Youku(url)
 
 print('\n' + 'Downloading video...')
-os.system('you-get ' + source + ' -O "' + title + '"-' + vidid)
+os.system('you-get ' + url + ' -O "' + title + '"-' + vidid)
 print('Done!')
