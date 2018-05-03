8d9b25433d5a65a2dd212fef0acca13ef4762aed
Gazlene <dkl3@users.noreply.github.com>
2018-05-03 00:42:16 +0000

    Add files via upload

--- youkuscrape.py
+++ youkuscrape.py
@@ -5,14 +5,29 @@ import json
 import wget
 import urllib.request
 from selenium import webdriver
+from requests.packages.urllib3.util import Retry
+from requests.adapters import HTTPAdapter
+from requests import Session, exceptions
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 
 vidid = input("Enter a Youku identifier: ")
 url = 'http://v.youku.com/v_show/id_' + vidid + '.html'
 fnameappend = '.info.json'
 jsonenc = json.JSONEncoder()
-page = urllib.request.urlopen(url)
-soup = BeautifulSoup(page, 'html.parser')
+browser = webdriver.PhantomJS() # be sure to sudo apt install phantomjs first!
+browser.set_page_load_timeout(10)
+while True:
+    try:
+        browser.get(url)
+    except TimeoutException:
+        print("Timeout, retrying...")
+        continue
+    else:
+        break
+
+soup = BeautifulSoup(browser.page_source, 'html.parser')
 title = soup.find("meta", {"name": "irTitle"})['content']
 
 def Youku(url):
@@ -25,7 +40,7 @@ def Youku(url):
 
     dict['channel'] = 'http:' + soup.find('span', attrs={'id': 'module_basic_sub'}).a['href']
     dict['uploaded'] = soup.find("span", {"class": "bold mr3"}).text.strip().replace('上传于 ','')
-    dict['uploader'] = soup.find("span", {"id": "module_basic_sub"}).text.strip()
+    dict['uploader'] = soup.find("dd", {"class": "dayu-title"}).text.strip()
 
     print("Title: " + title)
     print("Uploader: " + dict['uploader'])
